0e0ddc3300314e7a7996cbf15d0bbeac98e3ee7e
user <bb10956029@mail.npust.edu.tw>
2022-03-07 11:53:54 +0800

    金額

--- B10956029/Program.cs
+++ B10956029/Program.cs
@@ -11,6 +11,15 @@ namespace B10956029
         static void Main(string[] args)
         {
 
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+            int toto;
+
+
+            toto = (int)(meal_cost + tip_percent + tax_percent);
+
+            Console.WriteLine("總額:"+toto);
         }
     }
 }
